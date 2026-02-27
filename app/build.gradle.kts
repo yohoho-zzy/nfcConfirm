a4e2b477e99e31dccc72a8c5933b06cb7e1f424b
yohoho-zzy <1780524624@qq.com>
2026-02-27 14:56:41 +0900

    Refactor package structure and redesign login/NFC screens

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,11 +4,11 @@ plugins {
 }
 
 android {
-    namespace = "com.example.nfcconfirm"
+    namespace = "com.hitachi.confirmnfc"
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.example.nfcconfirm"
+        applicationId = "com.hitachi.confirmnfc"
         minSdk = 26
         targetSdk = 34
         versionCode = 1
