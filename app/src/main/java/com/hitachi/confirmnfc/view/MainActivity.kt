c4f155d59460322deef12cc12fe9c0161a16d520
zhangzhengyao <zhangzhengyao@bit-ware.cp.jp>
2026-03-04 17:03:54 +0900

    update

--- app/src/main/java/com/hitachi/confirmnfc/view/MainActivity.kt
+++ app/src/main/java/com/hitachi/confirmnfc/view/MainActivity.kt
@@ -1,7 +1,13 @@
 package com.hitachi.confirmnfc.view
 
+import android.content.Context
+import android.content.pm.ActivityInfo
 import android.os.Bundle
 import android.util.Log
+import android.view.MotionEvent
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import com.hitachi.confirmnfc.R
@@ -32,6 +38,37 @@ class MainActivity : AppCompatActivity() {
         Log.d(TAG, "onCreate savedInstanceState=${savedInstanceState != null}")
         mainViewModel.configureNavigator(supportFragmentManager, R.id.frameContainer)
         ensureInitialLoginPage()
+        requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
+    }
+
+    override fun dispatchTouchEvent(ev: MotionEvent): Boolean {
+        if (ev.action == MotionEvent.ACTION_DOWN) {
+            currentFocus?.let { hideKeyboardIfNeeded(it, ev) }
+        }
+        return super.dispatchTouchEvent(ev)
+    }
+
+    /**
+     * キーボードを非表示にする処理
+     */
+    private fun hideKeyboardIfNeeded(view: View, event: MotionEvent) {
+        if (view is EditText) {
+            val location = IntArray(2)
+            view.getLocationInWindow(location)
+
+            val left = location[0]
+            val top = location[1]
+            val right = left + view.width
+            val bottom = top + view.height
+
+            if (event.rawX < left || event.rawX > right ||
+                event.rawY < top || event.rawY > bottom
+            ) {
+                val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                imm.hideSoftInputFromWindow(view.windowToken, 0)
+                view.clearFocus()
+            }
+        }
     }
 
     /**
